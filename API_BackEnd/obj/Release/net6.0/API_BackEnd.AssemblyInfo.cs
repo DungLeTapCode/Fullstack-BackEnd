8a82d070f4f5b793168225022b3ec18f4db30fa3
DungLeTapCode <134661780+DungLeTapCode@users.noreply.github.com>
2024-12-16 10:16:06 +0700

    Mô tả thay đổi

--- API_BackEnd/obj/Release/net6.0/API_BackEnd.AssemblyInfo.cs
+++ API_BackEnd/obj/Release/net6.0/API_BackEnd.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_BackEnd")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78314113a600b9f4b99cc72f41d262160a70dc26")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_BackEnd")]
